052053b46efd978f677ae89b8ddf8d028537547d
jhpark <jhpark.vine@gmail.com>
2025-12-30 18:13:11 +0900

    stop reward 수정

--- colab/A2RL_a3c_colab.py
+++ colab/A2RL_a3c_colab.py
@@ -1560,7 +1560,7 @@ class Agent(threading.Thread):
             if action == 13:
                 # Penalty for stopping too early
                 if step < config.MIN_STEPS:
-                    reward = -1.0
+                    reward = -0.1
                     logger.info('Action 13 (STOP) triggered too early (step %d < %d) - Penalty: %.4f', 
                                 step, config.MIN_STEPS, reward)
                 else:
